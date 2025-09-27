795fe4cc45bd8302c815b27d02378fe1b37bf3a8
antisyst <redmiramazan@gmail.com>
2025-09-27 21:22:20 +0400

    pfpf

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import { StakeSection } from "@/components/StakeSection/StakeSection";
 import { Page } from "@/components/Page";
+import styles from './HomePage.module.scss';
 
 export const HomePage = () => {
   return (
     <Page back={false}>
+      <div className={styles.homePage}></div>
       <StakeSection/>
     </Page>
   )
