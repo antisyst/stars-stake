2a4ad3dbd971e4e7aaa966320590b66012a84a0d
antisyst <redmiramazan@gmail.com>
2025-10-27 00:46:27 +0400

    pfpf

--- src/components/UnsupportedPlatform/UnsupportedPlatform.tsx
+++ src/components/UnsupportedPlatform/UnsupportedPlatform.tsx
@@ -1,25 +1,26 @@
 import { QRCodeSVG } from 'qrcode.react';
 import styles from './UnsupportedPlatform.module.scss';
-import starsBaseLogoUrl from '../../../public/stars-base.png';
+import starsBaseLogoUrl from '@/assets/brand/stars-base-logo.svg?url';
 
 export function UnsupportedPlatform() {
   return (
     <div className={styles.unsupportedLayout}>
-      <QRCodeSVG
-        value="https://t.me/starsbase_bot/app"
-        size={160}
-        level="H"
-        bgColor="transparent"
-        fgColor="#ffff"
-        includeMargin={false}
-        title="Stars Base"
-        imageSettings={{
-          src: starsBaseLogoUrl,
-          height: 30,    
-          width: 30,
-          excavate: true,   
-        }}
-      />
+      <div className={styles.qrWrapper}>
+        <QRCodeSVG
+          value="https://t.me/starsbase_bot/app"
+          size={160}
+          level="H"
+          bgColor="transparent"
+          fgColor="#FFFF"
+          includeMargin={false}
+          title="Stars Base"
+        />
+        <img
+          src={starsBaseLogoUrl}
+          alt="Stars Base"
+          className={styles.qrLogo}
+        />
+      </div>
       <h1>Unsupported Platform</h1>
       <p>Only available on mobile. Please open it on your phone to continue.</p>
     </div>
