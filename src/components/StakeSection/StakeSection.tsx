83d5d0dfa8c2abc637e88c8df6a896565cd12762
antisyst <redmiramazan@gmail.com>
2026-04-24 22:34:19 +0400

    Initial commit

--- src/components/StakeSection/StakeSection.tsx
+++ src/components/StakeSection/StakeSection.tsx
@@ -15,7 +15,6 @@ import { primeIosFocusBridge } from '@/utils/iosFocusBridge';
 import { useCurrency } from '@/contexts/CurrencyContext';
 import { useRates } from '@/contexts/RatesContext';
 import { useI18n } from '@/i18n';
-import { SocialBonusSection } from '../SocialBonusSection/SocialBonusSection';
 import styles from './StakeSection.module.scss';
 import TonSymbolIcon from '@/assets/icons/toncoin-symbol.svg?react';
 
@@ -170,7 +169,6 @@ export const StakeSection: React.FC = () => {
           </div>
         )}
       </div>
-      <SocialBonusSection />
       <Modal
         isOpen={isApyOpen}
         title={t('stake.apyTitle')}
